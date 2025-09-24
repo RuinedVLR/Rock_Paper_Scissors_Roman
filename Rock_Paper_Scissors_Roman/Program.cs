1383976908241442ae1c7323ba814b75cc9f9149
RuinedVLR <rzhurakhov@gmail.com>
2025-09-24 15:31:30 -0300

    Initial commit

---
+++ Rock_Paper_Scissors_Roman/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors_Roman
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static string[] option = { "rock", "paper", "scissors" };
+        static string userInput = " ";
+        static int userWins = 0;
+        static int computerWins = 0;
+        static int computerChoice;
+
+
+        static void Main(string[] args)
+        {
+
+            while(userWins < 3 && computerWins < 3)
+            {
+                Console.WriteLine("Choose an option: ");
+                Console.WriteLine("1. Rock");
+                Console.WriteLine("2. Paper");
+                Console.WriteLine("3. Scissors");
+
+                computerChoice = random.Next(0, 2);
+
+                userInput = Console.ReadLine();
+                userInput.ToLowerInvariant();
+
+                Console.Clear();
+
+                if (userInput == null || userInput == "")
+                {
+                    Console.WriteLine("Invalid input, try again");
+                    Console.ReadKey(true);
+                }
+                else if (userInput != "rock" && userInput != "paper" && userInput != "scissors")
+                {
+                    Console.WriteLine("Invalid input, try again");
+                    Console.ReadKey(true);
+                }
+                else if (option[computerChoice] == userInput)
+                {
+                    Console.WriteLine("You chose: " + userInput);
+                    Console.WriteLine("Computer chose: " + option[computerChoice]);
+                    Console.WriteLine("Draw. No points earned.");
+                }
+                else if (option[computerChoice] == "rock" && userInput == "paper")
+                {
+                    UserWins();
+                }
+                else if (option[computerChoice] == "rock" && userInput == "scissors")
+                {
+                    ComputerWins();
+                }
+                else if (option[computerChoice] == "paper" && userInput == "scissors")
+                {
+                    UserWins();
+                }
+                else if (option[computerChoice] == "scissors" && userInput == "paper")
+                {
+                    ComputerWins();
+                }
+                else if (option[computerChoice] == "paper" && userInput == "rock")
+                {
+                    ComputerWins();
+                }
+                else if (option[computerChoice] == "scissors" && userInput == "rock")
+                {
+                    UserWins();
+                }
+                Console.ReadKey(true);
+                Console.Clear();
+            }
+            
+            if(userWins == 3)
+            {
+                Console.WriteLine("You are the winner! Score is " + userWins + " - " + computerWins);
+            }
+
+            if(computerWins == 3)
+            {
+                Console.WriteLine("Computer is the winner! Score is " + userWins + " - " + computerWins);
+            }
+        }
+
+        static void UserWins()
+        {
+            
+
+            Console.WriteLine("You chose: " + userInput);
+            Console.WriteLine("Computer chose: " + option[computerChoice]);
+            userWins++;
+            Console.WriteLine($"You win! Score is now: You {userWins} - Computer {computerWins}");
+        }
+
+        static void ComputerWins()
+        {
+            Console.WriteLine("You chose: " + userInput);
+            Console.WriteLine("Computer chose: " + option[computerChoice]);
+            computerWins++;
+            Console.WriteLine($"Computer win! Score is now: You {userWins} - Computer {computerWins}");
+        }
+    }
+}
